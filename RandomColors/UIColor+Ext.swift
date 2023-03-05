9104c3caf3afc90dfb782cab024a88ee5ec333eb
Dostonbek <bmister595@gmail.com>
2023-03-04 22:02:17 -0500

    hi

--- RandomColors/UIColor+Ext.swift
+++ RandomColors/UIColor+Ext.swift
@@ -9,6 +9,7 @@ import UIKit
 
 extension UIColor {
     
+    
     static func random() -> UIColor {
         let randomColor = UIColor(red: CGFloat.random(in: 0...1),
                                   green: CGFloat.random(in: 0...1),
